064272bf262ab73916f0179766da8563558c271b
Frida Bailleres <a01708633@tec.mx>
2024-02-29 12:44:15 -0600

    link pages to edit them

--- docusaurus.config.js
+++ docusaurus.config.js
@@ -40,7 +40,7 @@ const config = {
           // Please change this to your repo.
           // Remove this to remove the "edit this page" links.
           editUrl:
-            'https://github.com/facebook/docusaurus/tree/main/packages/create-docusaurus/templates/shared/',
+            'https://github.com/Black-Dot-2024/black-dot-wiki/edit/main/',
         },
         blog: false,
         theme: {
