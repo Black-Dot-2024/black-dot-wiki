3afdd9feef7812668a8b2a1992c6626467d0a40e
DavidLangarica <a01708936@tec.mx>
2024-02-29 09:38:46 -0600

    feat: add department info

--- docusaurus.config.js
+++ docusaurus.config.js
@@ -11,19 +11,13 @@ const config = {
   title: 'Black Dot',
   favicon: 'img/favicon.ico',
 
-  // Set the production url of your site here
   url: 'https://black-dot-2024.github.io',
   projectName: 'black-dot-wiki',
   organizationName: 'Black-Dot-2024',
   trailingSlash: false,
   baseUrl: '/black-dot-wiki',
-  deploymentBranch: 'gh-pages',
   onBrokenLinks: 'throw',
   onBrokenMarkdownLinks: 'throw',
-
-  // Even if you don't use internationalization, you can use this field to set
-  // useful metadata like html lang. For example, if your site is Chinese, you
-  // may want to replace "en" with "zh-Hans".
   i18n: {
     defaultLocale: 'es',
     locales: ['es'],
@@ -37,10 +31,9 @@ const config = {
         docs: {
           routeBasePath: '/',
           sidebarPath: './sidebars.js',
-          // Please change this to your repo.
-          // Remove this to remove the "edit this page" links.
           editUrl:
-            'https://github.com/facebook/docusaurus/tree/main/packages/create-docusaurus/templates/shared/',
+            // TODO: Change this to  repo
+            'https://github.com/Black-Dot-2024/black-dot-wiki/tree/main/docs',
         },
         blog: false,
         theme: {
@@ -53,17 +46,16 @@ const config = {
   themeConfig:
     /** @type {import('@docusaurus/preset-classic').ThemeConfig} */
     ({
-      // Replace with your project's social card
-      image: 'img/docusaurus-social-card.jpg',
+      image: 'img/logo-bd.png',
       navbar: {
         title: 'Black Dot',
         logo: {
           alt: 'Black Dot Logo',
-          src: 'img/logo.svg',
+          src: 'img/svg-bd.svg',
         },
         items: [
           {
-            href: 'https://github.com/facebook/docusaurus',
+            href: 'https://github.com/Black-Dot-2024',
             label: 'GitHub',
             position: 'right',
           },
@@ -71,35 +63,7 @@ const config = {
       },
       footer: {
         style: 'dark',
-        links: [
-          {
-            title: 'Community',
-            items: [
-              {
-                label: 'Stack Overflow',
-                href: 'https://stackoverflow.com/questions/tagged/docusaurus',
-              },
-              {
-                label: 'Discord',
-                href: 'https://discordapp.com/invite/docusaurus',
-              },
-              {
-                label: 'Twitter',
-                href: 'https://twitter.com/docusaurus',
-              },
-            ],
-          },
-          {
-            title: 'More',
-            items: [
-              {
-                label: 'GitHub',
-                href: 'https://github.com/facebook/docusaurus',
-              },
-            ],
-          },
-        ],
-        copyright: `Copyright © ${new Date().getFullYear()} My Project, Inc. Built with Docusaurus.`,
+        copyright: `Copyright © 2024 Black Dot. Built with Docusaurus.`,
       },
       prism: {
         theme: prismThemes.github,
