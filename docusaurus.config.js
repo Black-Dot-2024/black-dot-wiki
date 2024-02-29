17d4331eff9a383c8a36441f187b6758bda1248a
DavidLangarica <a01708936@tec.mx>
2024-02-28 22:05:14 -0600

    fix config file for actions

--- docusaurus.config.js
+++ docusaurus.config.js
@@ -16,8 +16,6 @@ const config = {
   projectName: 'black-dot-wiki',
   organizationName: 'Black-Dot-2024',
   trailingSlash: false,
-  // Set the /<baseUrl>/ pathname under which your site is served
-  // For GitHub pages deployment, it is often '/<projectName>/'
   baseUrl: '/black-dot-wiki',
   deploymentBranch: 'gh-pages',
   onBrokenLinks: 'throw',
