ce00a0e6f5296754cf5ec4c66ad5b466b8b5d07f
Frida Bailleres <a01708633@tec.mx>
2024-02-29 11:55:45 -0600

    change logo

--- docusaurus.config.js
+++ docusaurus.config.js
@@ -59,7 +59,7 @@ const config = {
         title: 'Black Dot',
         logo: {
           alt: 'Black Dot Logo',
-          src: 'img/logo.svg',
+          src: 'img/logo.png',
         },
         items: [
           {
